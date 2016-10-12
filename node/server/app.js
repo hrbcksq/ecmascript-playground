dda20542154aea1d998a20f3af3f02459c1316ec
hrbcksq <hrbcksq@gmail.com>
2016-10-12 13:19:46 +0300

    Simple json api

--- node/server/app.js
+++ node/server/app.js
@@ -2,10 +2,13 @@ var http = require("http");
 var fs = require('fs');
 
 http.createServer((req, res) => {
-
-    res.writeHead(200, { "Content-Type": "text/html" });    
-    fs.createReadStream(`${__dirname}/index.html`).pipe(res);
-
+    res.writeHead(200, { "Content-Type": "application/json" });
+    var obj = {
+        firstname: 'John',
+        lastname: 'Doe'
+    }    
+    res.write(JSON.stringify(obj));
+    res.end();
 }).listen(3005, '0.0.0.0');
 
 
