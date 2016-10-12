d8d26ddc07609eeb8017ceef2e17cd2e3565963a
hrbcksq <hrbcksq@gmail.com>
2016-10-12 12:45:29 +0300

    Simple template engine

--- node/server/app.js
+++ node/server/app.js
@@ -1,11 +1,18 @@
 var http = require("http");
+var fs = require('fs');
 
 console.log(this)
 http.createServer((req, res) => {
         res.writeHead(200, {
-        "Content-Type": "text/plain"
+        "Content-Type": "text/html"
     });
-    res.end("Plain text");
+
+    var message = 'Simple template engine';
+
+    fs.readFile(`${__dirname}/index.html`, 'utf-8', (err, data) => {
+        var data = data.replace('{Message}', message);
+        res.end(data);
+    });    
 }).listen(3005, '0.0.0.0');
 
 
