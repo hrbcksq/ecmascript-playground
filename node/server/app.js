66fd20920b670a27d8fec652076bd2e12fa64284
hrbcksq <hrbcksq@gmail.com>
2016-10-12 17:10:18 +0300

    Simple routing

--- node/server/app.js
+++ node/server/app.js
@@ -2,14 +2,21 @@ var http = require("http");
 var fs = require('fs');
 
 http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    var obj = {
-        firstname: 'John',
-        lastname: 'Doe'
-    }    
-    res.write(JSON.stringify(obj));
-    res.end();
-}).listen(3005, '0.0.0.0');
+        switch (req.url) {
+            case '/api':
+                res.writeHead(200, {
+                    'Content-Type' : 'application/json'
+                })
+                res.end(JSON.stringify(this));
+                break;
+            case '/site':
+                fs.createReadStream(`${__dirname}/index.htm`).pipe(res);
+                break;
+            default:
+                res.writeHead(404);
+                res.end();            
+        }    
+    }).listen(3005, '0.0.0.0');
 
 
 
