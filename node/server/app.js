a610a816fdaf35e04eb482cf134addb326846cd0
hrbcksq <hrbcksq@gmail.com>
2016-10-12 12:56:10 +0300

    Use stream instead read files

--- node/server/app.js
+++ node/server/app.js
@@ -1,18 +1,11 @@
 var http = require("http");
 var fs = require('fs');
 
-console.log(this)
 http.createServer((req, res) => {
-        res.writeHead(200, {
-        "Content-Type": "text/html"
-    });
 
-    var message = 'Simple template engine';
+    res.writeHead(200, { "Content-Type": "text/html" });    
+    fs.createReadStream(`${__dirname}/index.html`).pipe(res);
 
-    fs.readFile(`${__dirname}/index.html`, 'utf-8', (err, data) => {
-        var data = data.replace('{Message}', message);
-        res.end(data);
-    });    
 }).listen(3005, '0.0.0.0');
 
 
